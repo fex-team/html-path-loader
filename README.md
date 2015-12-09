9dacdd956dd0861c93000d07f576d6d84eea3193
dongtiancheng <dongtiangche@gmail.com>
2015-12-10 00:49:16 +0800

    更新文档

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ css 模块化工具
 
 此 loader 需要配合[css-path-loader](https://github.com/andycall/css-path-loader.git)一起使用才能生效
 
+scss 文件或者 less 文件必须和 react 组件文件必须放置在同一目录下,
+
+如果项目中有通用样式文件, 请使用 `@import` 来引入而不是在 `js` 使用 `require` 
+
+如果样式和组件是`完全分开`放置请不要使用这个 loader,  
+
 ## Usage
 
 在项目的 js 文件中, 为每一个react component 的 render 函数返回的 jsx 最外层加上  `_namespace` 参数,
@@ -17,7 +23,7 @@ className 属性.
     module.exports = React.createClass({
         render: function () {
             return (
-                <div _namespace> 
+                <div className="_namespace">
                     helloworld
                 </div>
             );
@@ -38,4 +44,14 @@ className 属性.
                 }
             ]
     }
+```
+
+## Tips
+
++ 如何方便设置 _namespace 本身的样式, 直接用`&` 就行了
+```css
+& {
+    // your code 
+}
+
 ```
--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 module.exports = function (source, map) {
     this.cacheable && this.cacheable()
 
-    // 找到入口文件绝对位置
-    var entryRelativePath = ''
-    if (Object.prototype.toString.call(this.options.entry) === '[object Array]') {
-        for (var value of this.options.entry) {
-            if (value.indexOf('webpack') === -1) {
-                entryRelativePath = value
-            }
-        }
-    } else {
-        entryRelativePath = this.options.entry
-    }
-
     // 得到了入口文件的绝对位置
-    var entryAbsolutePath = this.options.context + entryRelativePath.replace(/^\./g, '')
+    var entryAbsolutePath = this.options.context + '/'
 
     // 得到入口文件文件夹路径
     var entryAbsoluteFolderPathArray = entryAbsolutePath.split('/')
@@ -28,7 +16,7 @@ module.exports = function (source, map) {
 
     // 匹配 namespace
     source = source.replace(/(_namespace)/g, function (text, $1) {
-        return 'className="' + nameStr + '"'
+        return nameStr
     })
 
     this.callback(null, source, map)
