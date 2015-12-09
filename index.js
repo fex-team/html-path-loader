8fec6b6acc67b8ad0fc904d6f23d9638f001bf1b
dongtiancheng <dongtiangche@gmail.com>
2015-12-09 18:06:40 +0800

    修复 bug

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 module.exports = function (source, map) {
     this.cacheable && this.cacheable()
 
-    var namespace = this.resourcePath.replace(/[\W\w]+(?=src\b)/, '').replace(/\.scss/, '')
+    // 找到入口文件绝对位置
+    var entryRelativePath = ''
+    if (Object.prototype.toString.call(this.options.entry) === '[object Array]') {
+        for (var value of this.options.entry) {
+            if (value.indexOf('webpack') === -1) {
+                entryRelativePath = value
+            }
+        }
+    } else {
+        entryRelativePath = this.options.entry
+    }
+
+    // 得到了入口文件的绝对位置
+    var entryAbsolutePath = this.options.context + entryRelativePath.replace(/^\./g, '')
+
+    // 得到入口文件文件夹路径
+    var entryAbsoluteFolderPathArray = entryAbsolutePath.split('/')
+    entryAbsoluteFolderPathArray.pop()
+
+    var namespace = this.resourcePath.replace(entryAbsoluteFolderPathArray.join('/') + '/', '').replace(/\.(js|jsx)/, '')
 
     var nameArray = namespace.split('/')
     nameArray.pop()
-    namespace = nameArray.join('-')
+    var nameStr = nameArray.join('-')
 
-    // 豁免全局样式
-    if (namespace !== 'src') {
-        // 匹配 namespace
-        source = source.replace(/(namespace)/g, function (text, $1) {
-            return 'className="' + namespace + '"'
-        })
-    }
+    // 匹配 namespace
+    source = source.replace(/(_namespace)/g, function (text, $1) {
+        return 'className="' + nameStr + '"'
+    })
 
     this.callback(null, source, map)
 }
