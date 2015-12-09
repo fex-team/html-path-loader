14fca48970d64f47ab92f403dd20665242f9045a
AndyCall <dongtiangche@gmail.com>
2015-12-10 01:27:56 +0800

    修复 bug

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+var path = require('path')
+
 module.exports = function (source, map) {
     this.cacheable && this.cacheable()
+
+    // 对于 node_modules 里面的文件不能做处理
+    if (/node_modules/.test(this.resourcePath)) {
+        this.callback(null, source, map)
+        return false
+    }
+
+    // 找到入口文件绝对位置
+    if (Object.prototype.toString.call(this.options.entry) === '[object Array]') {
+        for (var value of this.options.entry) {
+            if (/[a-zA-Z-\.\/]+(js|jsx)$/.test(value)) {
+                var entryPath = path.resolve(this.options.context + '/' + value)
+                if (entryPath === this.resourcePath) {
+                    this.callback(null, source, map)
+                    return false;
+                }
+            }
+        }
+    }
 
     // 得到了入口文件的绝对位置
     var entryAbsolutePath = this.options.context + '/'
